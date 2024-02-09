7884673331037a55fa1420e476e596fa31bd389b
nyakamwe <nyakamweaimable@gmail.com>
2024-02-09 09:37:19 +0200

    feat(unit-test): implement unit testing and coverage

--- src/modules/customer/customer.service.spec.ts
+++ src/modules/customer/customer.service.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomerService } from './customer.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Customer } from './entities/customer.entity';
+import { RedisService } from '../redis/redis.service';
 
 describe('CustomersService', () => {
   let service: CustomerService;
 
+  const mockCustomerRepository = {
+    find: jest.fn(),
+    create: jest.fn(),
+    findOneBy: jest.fn(),
+  }
+
+  const  MockRedisService = {
+    get: jest.fn(),
+    set: jest.fn(),
+    del: jest.fn(),
+    flushdb: jest.fn()
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomerService],
+      providers: [
+        CustomerService,
+        RedisService,
+        { 
+          provide: getRepositoryToken(Customer),
+          useValue: mockCustomerRepository
+        },
+        {
+          provide: RedisService, 
+          useValue: MockRedisService, 
+          
+        },
+      ],
     }).compile();
 
     service = module.get<CustomerService>(CustomerService);
@@ -15,4 +43,5 @@ describe('CustomersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
 });
